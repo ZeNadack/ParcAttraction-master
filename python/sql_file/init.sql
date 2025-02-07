17e778f2792b1611179722286b4433eb3f59cb3c
ZeNadack <t.blanchard52000@gmail.com>
2025-02-07 09:15:19 +0100

    Création de la table critiques

--- python/sql_file/init.sql
+++ python/sql_file/init.sql
@@ -17,3 +17,17 @@ CREATE TABLE users (
     name varchar(255) not null,
     password varchar(255) not null
 );
+
+
+DROP TABLE IF EXISTS critiques;
+
+CREATE TABLE critiques (
+    critiques_id int auto_increment,
+    primary key(critiques_id),
+    attraction_id int not null,
+    foreign key (attraction_id) REFERENCES attraction(attraction_id),
+    texte varchar(255) not null,
+    note int not null,
+    nom varchar(255) null,
+    prenom varchar(255) null
+);
